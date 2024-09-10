21c22836372440c9f58e8c007e3d8fba0d1af87e
Cristian E. Nuno <cnuno2@illinois.edu>
2024-09-10 09:20:35 -0700

    feature/data-extraction-v2: Create service account postgres python user

--- scripts/postgres/init.sh
+++ scripts/postgres/init.sh
@@ -4,6 +4,17 @@ current_date=$(date +"%Y-%m-%d %H:%M:%S")
 
 echo "starting at ($current_date)"
 
-# NOTE: create the dedicated schema
-psql -U $POSTGRES_USER -d $POSTGRES_DB -v postgres_schema="$POSTGRES_SCHEMA" <<< "CREATE SCHEMA IF NOT EXISTS :postgres_schema;"
+# NOTE: create the service account user
+psql -U $POSTGRES_USER -d $POSTGRES_DB \
+    -v postgres_python_user=$POSTGRES_PYTHON_USER \
+    -v postgres_python_password=$POSTGRES_PYTHON_PASSWORD \
+    -v postgres_python_connection_limit=$POSTGRES_PYTHON_CONNECTION_LIMIT <<< \
+    "CREATE ROLE :postgres_python_user WITH LOGIN PASSWORD ':postgres_python_password' CONNECTION LIMIT :postgres_python_connection_limit;"
 
+# NOTE: ensure service account user can create different schemas
+psql -U $POSTGRES_USER -d $POSTGRES_DB \
+    -v postgres_db=$POSTGRES_DB \
+    -v postgres_python_user=$POSTGRES_PYTHON_USER <<< \
+    "GRANT CREATE ON DATABASE :postgres_db TO :postgres_python_user;"
+
+echo "all done at ($current_date)"
