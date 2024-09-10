4c934fd87a23439243f1fde235f66fbc28c5bc56
Cristian E. Nuno <cnuno2@illinois.edu>
2024-09-10 10:06:04 -0700

    feature/data-extraction-v2: Ensure service account can login via pgadmin

--- scripts/postgres/init.sh
+++ scripts/postgres/init.sh
@@ -9,7 +9,7 @@ psql -U $POSTGRES_USER -d $POSTGRES_DB \
     -v postgres_python_user=$POSTGRES_PYTHON_USER \
     -v postgres_python_password=$POSTGRES_PYTHON_PASSWORD \
     -v postgres_python_connection_limit=$POSTGRES_PYTHON_CONNECTION_LIMIT <<< \
-    "CREATE ROLE :postgres_python_user WITH LOGIN PASSWORD ':postgres_python_password' CONNECTION LIMIT :postgres_python_connection_limit;"
+    "CREATE ROLE :postgres_python_user WITH LOGIN PASSWORD :'postgres_python_password' CONNECTION LIMIT :postgres_python_connection_limit;"
 
 # NOTE: ensure service account user can create different schemas
 psql -U $POSTGRES_USER -d $POSTGRES_DB \
