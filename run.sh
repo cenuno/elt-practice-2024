62e032e9930b29738ad30a9e6785dc640f5ffc29
Cristian E. Nuno <cnuno2@illinois.edu>
2024-10-31 22:30:40 -0700

    feature/init_dbt: Update commands to run locally and to test in GHA

--- run.sh
+++ run.sh
@@ -11,16 +11,27 @@ echo "Ingest data into postgres..."
 docker exec -w /elt-practice-2024/src/elt_practice_2024 python python data_ingestion.py
 sleep 1
 
-echo "Install dbt dependencies..."
-docker exec -w /elt-practice-2024/src/elt_dbt python dbt deps
+echo "Clean out any existing DBT target folders..."
+docker compose run --rm dbt clean
 
-echo "Run DBT models..."
-docker exec -w /elt-practice-2024/src/elt_dbt python dbt run
+echo "Install DBT dependencies..."
+docker compose run --rm dbt deps
+
+echo "Ensure all DBT files can be parsed..."
+docker compose run --rm dbt parse
+
+echo "Build DBT models..."
+docker compose run --rm dbt build
 
 echo "Generate DBT docs..."
-docker exec -w /elt-practice-2024/src/elt_dbt python dbt docs generate
+docker compose run --rm dbt docs generate
 
-# echo "Serve DBT docs..."
-# docker exec -w /elt-practice-2024/src/elt_dbt python dbt docs serve
+echo "Spin up ngnix..."
+docker run -d -p 3000:80 dbt-docs-nginx
+
+echo "Copy DBT doc files into nginx..."
+docker cp ./src/elt_dbt/target/. nginx:/usr/share/nginx/html
+
+echo "You can now view DBT docs at http://localhost:3000/#!/overview. Enjoy the data lineage!"
 
 echo "All done!"
